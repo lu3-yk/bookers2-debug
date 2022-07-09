863d7aa080b189534e5dfa07db91394ff77a6aec
EC2 Default User <ec2-user@ip-172-31-15-51.ap-northeast-1.compute.internal>
2022-07-09 15:24:40 +0000

    chats_bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,18 +31,20 @@ class UsersController < ApplicationController
   end  
 
   def edit
-    @user=User.find(params[:id])
+    @user = user.find(params[:id])
    
   end
 
   def update
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render "edit"
     end
   end
+  
+  
 
   private
 
